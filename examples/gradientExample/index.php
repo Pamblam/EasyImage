b2b806599957c8d2f1b62e2c5ddbda33129aba7a
Robert Parham <adelphia@protonmail.ch>
2016-03-01 09:15:20 -0500

    fixed gradient example

--- examples/gradientExample/index.php
+++ examples/gradientExample/index.php
@@ -11,6 +11,6 @@ $here = realpath(dirname(__FILE__));
 
 $image = "$here/resources/apple.jpg";
 
-$gradientColors = EasyImage::gradientColors("#E05A65", "#7AA846", 50);
-
+$gradientColors = EasyImage::gradientColors(array("#E05A65", "#7AA846"), 50);
+echo count($gradientColors)." colors..<br>";
 foreach($gradientColors as $clr) echo "<div style='background:$clr; text-align:center;'>$clr</div>";
